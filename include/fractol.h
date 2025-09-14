f40f1d2c15f2183bcc6b37ae9cf99537f6391668
Mai Chi Tran Nhat <mtran-nh@3-I-4.42heilbronn.de>
2025-09-14 22:44:29 +0200

    some math functions

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: mtran-nh <mtran-nh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 20:49:46 by mtran-nh          #+#    #+#             */
-/*   Updated: 2025/09/14 20:52:30 by mtran-nh         ###   ########.fr       */
+/*   Updated: 2025/09/14 22:43:21 by mtran-nh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,24 @@
 
 # define FRACTOL_H
 
+# include "../Libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+
+#define ERROR_MSG "enter wrong name"
+
+typedef struct s_complex
+{
+	double	x;
+	double	y;
+}			t_complex;
+
+t_complex	sum_cmp(t_complex z1, t_complex z2);
+t_complex	sqrt_cmp(t_complex z);
+double		ft_atodbl(char *s);
 
 #endif
---
+++ src/math.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtran-nh <mtran-nh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/14 21:28:18 by mtran-nh          #+#    #+#             */
+/*   Updated: 2025/09/14 22:07:21 by mtran-nh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fractol.h"
+
+t_complex	sum_cmp(t_complex z1, t_complex z2)
+{
+	t_complex	result;
+
+	result.x = z1.x + z2.x;
+	result.y = z1.y + z2.y;
+	return (result);
+}
+
+t_complex	sqrt_cmp(t_complex z)
+{
+	t_complex	result;
+
+	result.x = (z.x * z.x) - (z.y * z.y);
+	result.y = 2 * z.x * z.y;
+	return (result);
+}
+
+double	ft_atodbl(char *s)
+{
+	long	int_part;
+	double	dec_part;
+	int		sign;
+	double	pow;
+
+	int_part = 0;
+	dec_part = 0;
+	sign = 1;
+	pow = 1;
+	while ((*s >= 9 && *s <= 13) || *s == 32)
+		s++;
+	while (*s == '+' || *s == '-')
+		if (*s++ == '-')
+			sign *= -1;
+	while (*s != '.' && *s)
+		int_part = (int_part * 10) + (*s++ - 48);
+	if (*s == '.')
+		s++;
+	while (*s)
+	{
+		pow /= 10;
+		dec_part = dec_part + (*s++ - 48) * pow;
+	}
+	return ((int_part + dec_part) * sign);
+}
